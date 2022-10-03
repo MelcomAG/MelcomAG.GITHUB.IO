174ad91f12c108322674461730a5bee1437d1cfa
croghostrider <vinko.varkas@gmail.com>
2022-10-03 17:05:01 +0200

    add electro staus

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,10 @@ export default function Home ({ lessons }) {
       </Head>
       <div style={{ backgroundPosition: '10px 10px' }} className="bg-slate-50 bg-grid-slate-100 min-h-screen">
         <div className="grid xl:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8 xl:px-96 md:px-32 p-4 justify-items-center">
-          {lessons.map((post) => (
+        <div className="text-3xl font-bold col-span-full">Insgesamt verfügbar { lessons.length } Parkplätze</div>
+          {lessons.map((post) => {
+            if (post.ist_elektro === true) {
+              return (
             <Fragment key={post.parkplatz_nr}>
               <div className="rounded-xl shadow-lg bg-white flex py-2 w-max pl-2 pr-1 space-x-1">
                 <div className="text-2xl font-bold text-black">Nr. {post.parkplatz_nr}</div>
@@ -24,8 +27,16 @@ export default function Home ({ lessons }) {
                   <BoltIcon className="h-8 w-8 text-green-300" />
                 </div>
               </div>
+            </Fragment>)
+            }
+            return (
+              <Fragment key={post.parkplatz_nr}>
+              <div className="rounded-xl shadow-lg bg-white flex p-2">
+                <div className="text-2xl font-bold text-black">Nr. {post.parkplatz_nr}</div>
+              </div>
             </Fragment>
-          ))}
+            )
+          })}
         </div>
       </div>
     </>
@@ -35,7 +46,7 @@ export default function Home ({ lessons }) {
 export const getStaticProps = async () => {
   const { data: lessons } = await supabase
     .from('TTN')
-    .select('*')
+    .select('parkplatz_nr,ist_elektro')
     .is('parkplatz_belegt', false)
     .order('parkplatz_nr', { ascending: true })
   return {
