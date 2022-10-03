41bddde6b421c45776a80b3c107721d05569c978
croghostrider <vinko.varkas@gmail.com>
2022-10-03 15:29:16 +0200

    fix build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,28 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  images: {
-    unoptimized: true
-  },
-  reactStrictMode: true,
-  swcMinify: true
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+let nextConfig = {}
+
+if (isGithubActions) {
+  const assetPrefix = './'
+  const basePath = ''
+  nextConfig = {
+    assetPrefix,
+    basePath,
+    images: {
+      unoptimized: true
+    },
+    reactStrictMode: true,
+    swcMinify: true
+  }
+} else {
+  nextConfig = {
+    images: {
+      unoptimized: true
+    },
+    reactStrictMode: true,
+    swcMinify: true
+  }
 }
 
 module.exports = nextConfig
