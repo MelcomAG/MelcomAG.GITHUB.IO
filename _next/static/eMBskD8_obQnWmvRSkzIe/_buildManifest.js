46d3a11c605c43955aa20ca0ca31be9431e0d91d
croghostrider <cro.ghost.rider@gmail.com>
2022-10-03 08:37:28 +0000

    Deploying to gh-pages from @ MelcomAG/MelcomAG.GITHUB.IO@86fedd51ae0eb760fad0c19d12af2515f86158f3 🚀

---
+++ _next/static/eMBskD8_obQnWmvRSkzIe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7f1376d225dbb74e.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
